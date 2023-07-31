9f76362355ba2558b13835756f3024db6a3fa072
Pavan Narra <pavan@teqsar.com>
2023-07-31 18:54:56 +0530

    schedular properties file changes Done

--- src/main/java/com/mfino/digilinq/scheduler/enitiy/SchedulerJobInfo.java
+++ src/main/java/com/mfino/digilinq/scheduler/enitiy/SchedulerJobInfo.java
@@ -1,5 +1,6 @@
 package com.mfino.digilinq.scheduler.enitiy;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,22 +15,24 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table( name = "dgl_scheduler_job_info")
+@Table( name = "dgl_qrtz_scheduler_job_info")
 public class SchedulerJobInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id")
     private Long id;
 
+    @Column(name ="job_name")
     private String jobName;
-
+    @Column(name ="job_group")
     private String jobGroup;
-
+    @Column(name ="job_class")
     private String jobClass;
-
+    @Column(name ="cron_expression")
     private String cronExpression;
-
+    @Column(name ="repeat_time")
     private Long repeatTime;
-
+    @Column(name ="cron_job")
     private Boolean cronJob;
 }
